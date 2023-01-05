db09d7c878105701aad2e9d09687c6efc730bfd9
Mehriban-Ali-Seyidbayli <mehriban.ali.seyidbayli.94@gmail.com>
2023-01-05 17:29:18 +0400

    first commit

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '370cae1365msh6cd6ad328d93bdcp193545jsn35ebee6c9df4',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "390106e7543d4166f17df15051cbb280";
+
